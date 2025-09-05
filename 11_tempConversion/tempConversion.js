a62ae24f251530402617db60a428da59397e3412
cyber-Ray <112042471+cyber-Ray@users.noreply.github.com>
2025-09-05 10:45:43 -0500

    Passed tests through using parseFloat and toFixed

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  celsius = (temperature - 32) * 5 / 9;
+  return parseFloat(celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  fahrenheit = temperature * 9 / 5 + 32;
+  return parseFloat(fahrenheit.toFixed(1))
 };
 
 // Do not edit below this line
