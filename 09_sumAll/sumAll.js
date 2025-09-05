eef11d7f3175db63ce2dfd2364c148ef0cbafaa1
cyber-Ray <112042471+cyber-Ray@users.noreply.github.com>
2025-09-04 22:26:35 -0500

    Passed tests by creating basic loop

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+  if (!Number.isInteger(start) || !Number.isInteger(end) || start < 0 || end < 0) {
+    return "ERROR";
+  }
 
+  let total = 0;
+  if (start > end) {
+    const temp = start;
+    start = end;
+    end = temp;
+  }
+
+  for (let i = start; i <= end; i++) {
+  total += i;
+  }
+  return total;
 };
 
 // Do not edit below this line
