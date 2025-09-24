05299ff7030f1a62be95340badfa8ff363d5497e
cyber-Ray <112042471+cyber-Ray@users.noreply.github.com>
2025-09-23 19:37:07 -0500

    Passed tests through basic math and iterating through object

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(first, second) {
+	return first + second;
 };
 
-const subtract = function() {
-	
+const subtract = function(first, second) {
+	return first - second;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let total = 0;
+	for (const num of array) {
+    total += num;
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let total = 1;
+	for (const num of array) {
+    total *= num;
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(first, second) {
+	return first ** second;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0) {
+    return 1;
+  } else {
+    let total = 1;
+    for (let i = 1; i <= num; i++) {
+      total *= i;
+    }
+    return total;
+  }
 };
 
 // Do not edit below this line
