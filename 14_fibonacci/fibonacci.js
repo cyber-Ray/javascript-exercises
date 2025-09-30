e8686ecfef849728b64c67d9db5960e1ff0cc98f
cyber-Ray <112042471+cyber-Ray@users.noreply.github.com>
2025-09-30 11:09:21 -0500

    Passed tests through creating array of sequence

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(count) {
+  let sequence = [0, 1];
+  let length;
+  if(typeof count !== 'number') {
+    length = parseInt(count);
+  } else {
+    length = count;
+  }
 
+  if (length < 0) {
+    return "OOPS"
+  }
+
+  for(let i = 2; i <= length; i++) {
+    sequence[i] = sequence[i-2] + sequence[i-1];
+  }
+  return sequence[length];
 };
 
 // Do not edit below this line
