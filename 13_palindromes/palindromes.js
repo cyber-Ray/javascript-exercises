a7988a05b5223569e853325f19a08d5cdb659b95
cyber-Ray <112042471+cyber-Ray@users.noreply.github.com>
2025-09-30 10:30:50 -0500

    Passed tests through array manipulation

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const valid = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+  const cleaned = string.toLowerCase().split('').filter(char => valid.includes(char)).join('');
+  const reversed = cleaned.split('').reverse().join('');
+
+  return cleaned === reversed;
 };
 
 // Do not edit below this line
